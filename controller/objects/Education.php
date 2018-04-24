a415228ab2984603a277dda28fd737f082a3e543
AdekWhat <=>
2018-04-24 21:31:50 +0400

    Aded abstract class Label, now Temlate call instance of classes instead of variables

---
+++ controller/objects/Education.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AdekWhat
+ * Date: 24.04.2018
+ * Time: 18:41
+ */
+
+namespace controller\objects;
+
+
+class Education
+{
+
+}
---
+++ controller/objects/PersonalData.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AdekWhat
+ * Date: 24.04.2018
+ * Time: 18:37
+ */
+
+class PersonalData
+{
+
+}
